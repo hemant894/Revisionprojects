9296ecb4732979a17fc9d438b1da9116cbbdac6b
Hemant <hemant894@gmail.com>
2022-12-20 18:46:10 +0000

    Revisionprojects

---
+++ RevisionProject.java
@@ -0,0 +1,29 @@
+package Revisionproject;
+
+public class RevisionProject {
+    public static void main(String[] args) {
+        System.out.println(" What is version control?");
+        System.out.println(" Version control is a system that records changes to a file or set of files over time so that you can recall specific version later.");
+        System.out.println(" What is Repository?");
+        System.out.println(" A place , room or container where something is deposited or stored.");
+        System.out.println(" .........Concept of Git........");
+        System.out.println(" Keep track of code history.");
+        System.out.println(" Takes' Snapshots ' of your files ");
+        System.out.println(" You decide when to take a snapshot by making a 'Commit'");
+        System.out.println(" You can visit any snapshot at any time.");
+        System.out.println(" You can stage files before committing.");
+        System.out.println(" Git Commands-1");
+        System.out.println(" git init");
+        System.out.println(" git add .");
+        System.out.println(" git commit -m 'Revisionproject' ");
+        System.out.println(" git branch -m master main ");
+        System.out.println(" git remote add origin <Repository URL>");
+        System.out.println(" git push -u origin main");
+        System.out.println(" @@@@@@ Git Commands-2");
+        System.out.println( " git pull or git fetch");
+        System.out.println(" git reset ");
+        System.out.println(" That's done!");
+        System.out.println(" Second Attempt");
+
+    }
+}
